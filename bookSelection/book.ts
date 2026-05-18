b244cea98fee75d7ba1383a1d0cf25680481f159
samuelalves21 <samuelalves21@gmail.com>
2026-05-18 21:57:57 +0100

    push all code

---
+++ bookSelection/book.ts
@@ -0,0 +1,66 @@
+import {Page, expect} from '@playwright/test'
+
+export class Book {
+    
+    readonly page: Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async searchBook (name: string, author: string) {
+        //com este método, é possível com base no nome do livro e no autor, encontrar o resultado pretendido. Escolhe o primeiro livro da lista 
+        const searchBook = this.page.getByPlaceholder('pesquisar')
+        await searchBook.click()
+        await searchBook.fill(name)
+        await this.page.locator('[aria-label="pesquisar"]').click()
+        await this.page.locator(`[data-product-name="${name}"]`, {hasText: author}).locator('.track').first().click()
+    }
+
+
+    async compareAuthors(bookName1: string, bookName2: string) {
+        const searchBook = this.page.getByPlaceholder('pesquisar')
+        await searchBook.click()
+        await searchBook.fill(bookName1)
+        await this.page.locator('[aria-label="pesquisar"]').click()
+        await this.page.locator(`[data-product-name="${bookName1}"]`, {hasText: "George Orwell"}).locator('.track').first().click()
+        const authorName = await this.page.locator('#productPageRightSectionTop-author-lnk', {hasText: "George Orwell"}).textContent()
+
+        await this.page.goBack()
+        await searchBook.click()
+        await searchBook.fill(bookName2)
+        await this.page.locator('[aria-label="pesquisar"]').click()
+        await this.page.locator(`[data-product-name="${bookName2}"]`, {hasText: "George Orwell"}).locator('.track').first().click()
+        const authorName2 = await this.page.locator('#productPageRightSectionTop-author-lnk', {hasText: "George Orwell"}).textContent()
+        expect(authorName2).toEqual(authorName)
+    }
+
+    async validateAuthor(author: string) {
+        const bookAuthor = this.page.locator('#productPageRightSectionTop-author-lnk') //localiza o autor
+        await expect(bookAuthor).toHaveText(author) //confirma o nome do autor
+    }
+
+    async validateISBN(isbn: string) {
+        const bookISBN = this.page.locator('#productPageSectionDetails-collapseDetalhes-content-isbn', {hasText: "ISBN:"}).locator('.info') //localiza o ISBN
+        await expect(bookISBN).toHaveText(isbn) //confirma o ISBN
+    }
+
+    async validatePages(pages: string) {
+        const bookPages = this.page.locator('#productPageSectionDetails-collapseDetalhes-content-nrPages', {hasText: "Páginas:"}).locator('.info') //localiza o numPaginas
+        await expect(bookPages).toHaveText(pages) //confirma o número páginas
+    }
+
+    async validateDimension(dimension: string) {
+         const bookDimension = this.page.locator('#productPageSectionDetails-collapseDetalhes-content-dimensions', {hasText: "Dimensões:"}).locator('.info') //localiza as dimensões
+        await expect(bookDimension).toHaveText(dimension) //confirma as dimensões do livro
+    }
+    
+    async validateLanguage()  {
+        const bookLanguage = this.page.locator('#productPageRightSectionTop-language') //localiza o idioma
+        await expect(bookLanguage).toContainText('Inglês') //confirma se o Idioma é Inglês
+    }
+
+    async validateFlag() {
+        await expect(this.page.locator('#productPageRightSectionTop-languageFlag')).toHaveClass('icon language-flag Inglês') //confirma se a bandeira é do UK
+    }
+}
--- tests/bertrand.spec.ts
+++ tests/bertrand.spec.ts
@@ -1,2 +1,56 @@
 import { test, expect } from '@playwright/test';
+import {Book} from '../bookSelection/book'
 
+/*Scenario 1:
+
+Search for the book "1984."
+Validate that the author is "George Orwell."
+Confirm that the ISBN is "9789722071550."
+Check that the number of pages is "344."
+Ensure that the dimensions of the book are "156 x 238 x 22 mm."*/
+
+test.beforeEach(async({page}) => {
+    await page.goto("https://www.bertrand.pt/")
+})
+
+    test('Scenario 1', async({page}) => {
+        test.setTimeout(10000)
+
+        const book = new Book(page)
+        await book.searchBook("1984", "George Orwell")
+    
+        await book.validateAuthor('George Orwell')
+        await book.validateISBN('9789722071550')
+        await book.validatePages('344')
+        await book.validateDimension('156 x 238 x 22 mm')
+    })
+
+/*Scenario 2:
+
+Search for the book "1984."
+Verify that the book "A Quinta dos Animais" is authored by the same author.*/
+
+    test('Scenario 2', async({page}) => {
+        test.setTimeout(10000)
+
+        const book = new Book(page)
+        await book.compareAuthors('1984', 'A Quinta dos Animais')      
+    })
+
+/*Scenario 3:
+
+Search for the book "Do Not Disturb."
+Validate that the author is "Freida McFadden"
+Validate the idiom is "Inglês" and the flag of UK is displayed*/
+
+
+    test('Scenario 3', async({page}) => {
+        test.setTimeout(10000)
+
+        const book = new Book(page)
+        await book.searchBook('Do Not Disturb', 'Freida McFadden')      
+
+        await book.validateAuthor('Freida McFadden')
+        await book.validateLanguage()
+        await book.validateFlag()    
+    })
